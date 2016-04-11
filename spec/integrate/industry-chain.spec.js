fae3c3849d34715ebd5db3fa65b4dc7df019779e
Winton Welsh <mail@wintoni.us>
2016-04-11 00:11:23 -0700

    Chain extension + spec

--- spec/integrate/industry-chain.spec.js
+++ spec/integrate/industry-chain.spec.js
@@ -1,7 +1,97 @@
-import IndustryChain from "../../"
+import { factory } from "industry"
+import { instance } from "industry-instance"
+import { functions } from "industry-functions"
+import { standard_io } from "industry-standard-io"
+import { chain} from "../../"
 
 describe("IndustryChain", () => {
-  it("works", () => {
+  function makeTest() {
+    return factory()
+      .set("instance", instance)
+      .set("functions", functions)
+      .set("standard_io", standard_io)
+      .set("chain", chain)
+  }
+
+  it("implements the chain function", (done) => {
+    let base = class {
+      a() { return { a: 1 } }
+      b() { return { b: 2 } }
+      c({ promise: { resolve } }) { setTimeout(() => resolve({ c: 3 }), 1) }
+      d({ promise: { resolve } }) { resolve({ d: 4 }) }
+      e() { return { e: 5 } }
+      f({ promise: { resolve } }) { resolve({ f: 6 }) }
+      empty({ value }) { return value || true }
+      chain({ promise: { chain } }) { return chain(this.c, this.empty, this.d) }
+
+      run({ promise: { chain } }) {
+        return chain(
+          chain(this.a, this.empty),
+          this.b, this.chain, this.e, this.f
+        )
+      }
+    }
+
+    let test = makeTest().base(base)
+    let value = test().run()
+    let expected = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, async: true, value: { b: 2 } }
     
+    value.then(args => {
+      expect(args).toEqual(expected)
+      done()
+    })
+
+    delete value.then
+    expect(value).toEqual({
+      a: 1, b: 2, async: true, value: { b: 2 }
+    })
+  })
+
+  it("returns a synchronous value from the chain function", () => {
+    let base = class {
+      a() { return "a" }
+      b({ promise: { resolve } }) { resolve("b") }
+
+      run({ promise: { chain } }) {
+        return chain(this.a, this.b)
+      }
+    }
+
+    let test = makeTest().base(base)
+    expect(test().run().value).toBe("b")
+  })
+
+  it("allows chains from multiple functions", (done) => {
+    let base = class {
+      a() { return { a: 1 } }
+      b() { return { b: 2 } }
+      c() { return { c: 3 } }
+      d() { return { d: 4 } }
+
+      chain({ promise: { chain } }) {
+        return chain(this.c, this.d)
+      }
+
+      chain2({ promise: { chain } }) {
+        return chain(this.a, this.b, this.chain)
+      }
+
+      run({ promise: { chain } }) {
+        return this.chain2()
+      }
+    }
+
+    let test = makeTest().base(base)
+    
+    let output = test().run()
+    let expected = { a: 1, b: 2, c: 3, d: 4, value: { d: 4 } }
+
+    output.then(args => {
+      expect(args).toEqual(expected)
+      done()
+    })
+
+    delete output.then
+    expect(output).toEqual(expected)
   })
 })
--- src/industry-chain.js
+++ src/industry-chain.js
@@ -1,3 +1,81 @@
-export default class IndustryChain {
-  
+import { returnObject } from "standard-io"
+
+function each({ args }) {
+  return (...chains) => {
+    let rand = Math.floor(Math.random() * (100 + 1))
+    let promise
+
+    chains.forEach(c => {
+      if (promise) {
+        promise = promise
+          .then(() => c(args))
+          .then(value => {
+            mergeArgs({ args, value })
+            return args
+          })
+      } else {
+        if (typeof c == "function") {
+          c = c(args)
+        }
+        if (!c) {
+          // do nothing
+        } else if (c.async || (c.value == undefined && c.then)) {
+          args.async = true
+          promise = c.then(value => {
+            mergeArgs({ args, value })
+            return args
+          })
+        } else {
+          mergeArgs({ args, value: c })
+        }
+      }
+    })
+
+    promise = promise || Promise.resolve(args)
+    let output = returnObject({ value: args })
+    output.then = promise.then.bind(promise)
+
+    return output
+  }
 }
+
+function mergeArgs({ args, value={} }) {
+  if (typeof value == "object") {
+    for (let key in value) {
+      if (key != "then") {
+        args[key] = value[key]
+      }
+    }
+  } else {
+    args.value = value
+  }
+}
+
+function patch(ignore) {
+  for (let name in this.functions()) {
+    if (ignore.indexOf(name) == -1) {
+      let fn = this[name]
+      this[name] = (args) =>
+        args.chain = {
+          each: each({ args: args.args })
+        }
+    }
+  }
+}
+
+export let chain = Class =>
+  class extends Class {
+    static beforeFactoryOnce() {
+      this.industry({
+        ignore: {
+          args: [ "chain" ]
+        }
+      })
+      super.beforeFactoryOnce()
+    }
+
+    beforeInit() {
+      patch.bind(this)(this.Class.industry().ignore.instance)
+      super.beforeInit()
+    }
+  }
